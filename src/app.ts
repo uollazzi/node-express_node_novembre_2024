668eb9e9e60f6b662eb6e3a5b7305ace743a2a90
Matteo Valenzi <dilloawallace@hotmail.com>
2024-11-20 21:16:04 +0100

    hbs

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,42 @@
 import express, { Request, Response, NextFunction } from "express";
+import { DateTime } from "luxon";
 
 const port = 3000;
 const app = express();
 
+// configurazione template engine
+app.set("views", "./src/views");
+app.set("view engine", "hbs");
 
+// middleware 
+app.use((req: Request, res: Response, next: NextFunction) => {
+    console.log("LOG:", req.method, req.url);
+    next();
+});
 
+// black friday
+app.use((req: Request, res: Response, next: NextFunction) => {
+    const adesso = DateTime.local();
+    if (adesso.month == 11 && adesso.day == 22) {
+        res.send("BLACK FRIDAY");
+    }
+    next();
+});
 
+// MVC ???
+app.get("/", (req: Request, res: Response) => {
+    res.render("index", { nome: req.query.nome, pageTitle: "Benvenuti da me" });
+});
 
+app.get("/benvenuto", (req: Request, res: Response) => {
 
+    res.send("Benvenuto");
+});
+
+app.get("/api/pippo", (req: Request, res: Response) => {
+    res.json({ nome: "Pippo", anni: 34 });
+
+});
 
 
 
